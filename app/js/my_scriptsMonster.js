a6b13ea38363df1bbffdcf4a929de26b7058786f
DimiG <dim.georgiev1976@yandex.com>
2019-01-27 20:51:23 +0000

    End chapter 6

--- app/js/my_scriptsMonster.js
+++ app/js/my_scriptsMonster.js
@@ -1,68 +1,73 @@
 $(document).ready(function(){
 
-		var headclix = 0,eyesclix = 0,noseclix = 0,mouthclix = 0;
+		var headclix = 0, eyeclix = 0, noseclix = 0, mouthclix = 0;
+
+		// вызываются функции,выделенные жирным шрифтом в конце
+	   	 lightning_one(4000);
+  		 lightning_two(100000);
+		 lightning_three(17000);
 
 	$("#head").click(function(){
 		// Условие ограничивает пользователя девятью 
-		if (headclix < 9) {
-		// Здесь будет размещаться код анимации.
-
+		if (headclix < 9) {	
+			$(this).animate({left:"-=367px"},500);
 			headclix += 1;
 		} else {
-		// Здесь будет размещаться код «перемотки» полосы изображений
-			
-
-			headclix = 0;
-		}
+			$(this).animate({left:"0px"},500);
+			headclix = 0;	}
 	});// end click	
 
+
+
+
 	$("#eyes").click(function(){
-			// Условие ограничивает пользователя девятью 
-			if (eyesclix < 9) {
-			// Здесь будет размещаться код анимации.
-
-				eyesclix += 1;
+			if (eyeclix < 9) {
+				$(this).animate({left:"-=367px"},500)
+					eyeclix += 1;
 			} else {
-			// Здесь будет размещаться код «перемотки» полосы изображений
-				
-
-				eyesclix = 0;
+				$(this).animate({left:"0px"},500);
+			eyeclix = 0; 
 			}
 		});// end click
-
-	$("#nose").click(function(){
+ 	$("#nose").click(function(){
 			// Условие ограничивает пользователя девятью 
 			if (noseclix < 9) {
-			// Здесь будет размещаться код анимации.
-
+				$(this).animate({left:"-=367px"},500);
 				noseclix += 1;
 			} else {
-			// Здесь будет размещаться код «перемотки» полосы изображений
-				
-
-				noseclix = 0;
-			}
+				$(this).animate({left:"0px"},500);			
+			noseclix = 0; }
 		});// end click
 
 	$("#mouth").click(function(){
-		// Условие ограничивает пользователя девятью 
-		if (mouthclix < 9) {
-		// Здесь будет размещаться код анимации.
-
+		if (mouthclix < 9) { 
+			$(this).animate({left:"-=367px"},500);
 			mouthclix += 1;
 		} else {
-		// Здесь будет размещаться код «перемотки» полосы изображений
-			
-
-			mouthclix = 0;
-		}
+			$(this).animate({left:"0px"},500);
+			mouthclix = 0;	}
 	});// end click
+});//end doc.onready function
+
+		// Определения функций сверкания молний
+		function lightning_one(){
+
+		$("#container #lightning1").fadeIn(250).fadeOut(250);
+		setTimeout("lightning_one()",3000);
+		};// end of lightning_one
+
+		// lightning_two
+		function lightning_two(){
+			$("#container #lightning2").fadeIn("fast").fadeOut("fast");
+			setTimeout("lightning_two()",10000);
+		};// end of lightning_two
 
 
-		function lightning_one(t){
+		// lightning_three
+		function lightning_three(){
+			$("#container #lightning3").fadeIn("fast").fadeOut("fast");
+			setTimeout("lightning_two()",5000);
+		};// end of lightning_three
 
-		$("#lightning1").fadeIn("250").fadeOut("250");
-		setTimeout("lightning_one(), t");
-		};
+// https://resources.oreilly.com/examples/0636920012740/tree/master/ch05/begin/images
 
-});// end ready document
